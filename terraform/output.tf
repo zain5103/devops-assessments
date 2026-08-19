cca89c3bf17f59b04d22d87a24bd8eec0409bea9
zain5103 <muhammadzain8489@gmail.com>
2026-08-20 00:52:04 +0500

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,19 +1,19 @@
-output "jenkins_master_public_ip" {
-  value       = data.aws_instance.jenkins_master.public_ip
-  description = "Public IP of existing Jenkins Master"
+output "jenkins_master_url" {
+  value       = "http://${aws_instance.jenkins_master.public_ip}:8080"
+  description = "Access Jenkins Master Dashboard"
 }
 
 output "jenkins_agent_public_ip" {
-  value       = data.aws_instance.jenkins_agent.public_ip
-  description = "Public IP of existing Jenkins Agent (Deploy Server)"
+  value       = aws_instance.jenkins_agent.public_ip
+  description = "Public IP of Agent Node (Deployment Target)"
+}
+
+output "alb_dns_name" {
+  value       = "http://${aws_lb.main.dns_name}"
+  description = "Application Load Balancer URL"
 }
 
 output "s3_backup_bucket" {
   value       = aws_s3_bucket.app_backups.id
-  description = "S3 Bucket created for backups"
-}
-
-output "cloudwatch_log_group" {
-  value       = aws_cloudwatch_log_group.app_logs.name
-  description = "CloudWatch log group created for application"
+  description = "Backup S3 Bucket Name"
 }
