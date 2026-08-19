9de24259bfa4ae5350a3e6e89abc461cb42970b6
zain5103 <muhammadzain8489@gmail.com>
2026-08-20 01:11:28 +0500

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,15 +5,15 @@ output "jenkins_master_url" {
 
 output "jenkins_agent_public_ip" {
   value       = aws_instance.jenkins_agent.public_ip
-  description = "Public IP of Agent Node (Deployment Target)"
+  description = "Public IP of Deployment Agent Server"
 }
 
 output "alb_dns_name" {
   value       = "http://${aws_lb.main.dns_name}"
-  description = "Application Load Balancer URL"
+  description = "Application Load Balancer CNAME URL"
 }
 
 output "s3_backup_bucket" {
   value       = aws_s3_bucket.app_backups.id
-  description = "Backup S3 Bucket Name"
+  description = "S3 Backup Bucket Name"
 }
