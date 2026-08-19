28b3a9968ed94815af63200225f4541a9c7cc245
zain5103 <muhammadzain8489@gmail.com>
2026-08-20 00:41:55 +0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
+  type        = string
   default     = "eu-north-1"
-  description = "AWS Region where existing instances are running"
+  description = "AWS Region for deployment"
 }
 
 variable "environment" {
+  type        = string
   default     = "staging"
   description = "Environment name"
 }
 
-# Master Jenkins instance ID (Inbound/Outbound references ke liye)
-variable "jenkins_master_instance_id" {
+variable "instance_type" {
   type        = string
-  default     = "i-00fdf5361782e07fc" # Apni AWS Console se Jenkins Master ki Instance ID lagayein
-  description = "Instance ID of existing Jenkins Master"
+  default     = "t3.small"
+  description = "EC2 Instance type for Agent"
 }
 
-# Jenkins Agent / Deployment instance ID
-variable "jenkins_agent_instance_id" {
+variable "master_instance_type" {
   type        = string
-  default     = "i-0848ceb55e5a3e438" # Apni AWS Console se Jenkins Agent ki Instance ID lagayein
-  description = "Instance ID of existing Jenkins Agent/Deployer"
+  default     = "t3.small"
+  description = "EC2 Instance type for Jenkins Master"
 }
