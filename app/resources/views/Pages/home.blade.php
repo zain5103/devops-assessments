7ad5226abdee88227d7ab43f921d2af336a80f12
zain5103 <muhammadzain8489@gmail.com>
2026-08-20 23:43:14 +0500

    Update home.blade.php

--- app/resources/views/Pages/home.blade.php
+++ app/resources/views/Pages/home.blade.php
@@ -6,7 +6,7 @@
 
         <h1
             class="text-6xl md:text-8xl font-extrabold tracking-tighter text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-500 mb-6">
-            Think and Build. <br>Create.
+            Think && Build. <br>Create.
         </h1>
         <p class="text-gray-400 text-lg max-w-2xl mb-10 leading-relaxed">
             The minimal space to capture your most ambitious thoughts. Simple, dark, and focused.
